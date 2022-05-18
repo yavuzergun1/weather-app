99ea4e17f789b1d05a1e83975a5e58b21c312269
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-05-18 21:09:35 +0300

    commited

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import Data from './components/data'
-
+import axios from 'axios';
+import { useState, useEffect } from 'react'
+import { usePosition } from 'use-position'
 
 function App() {
 
+  const [weather, setweather] = useState([]);
+   
+    const {latitude, longitude}= usePosition();
+    console.log(latitude, longitude);
  
+    useEffect(()=>{
+      axios(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=1ac2c05c20ed62694dfdbe712be26cae`)
+      .then((res)=> (setweather(res.data)))
+      .catch((e)=>console.log(e))
+     
+      console.log(weather);
+      
+      }, [latitude])
+  
+
 
   return (
     <div>
-   <Data/>
+    {JSON.stringify(weather)}
     </div>
-  );
+  )
 }
+
 
 export default App;
