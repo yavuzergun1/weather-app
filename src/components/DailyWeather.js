9bd8d0fe305324b9105983800b8c99daffb5b5c2
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-05-23 22:20:38 +0300

    commited

--- src/components/DailyWeather.js
+++ src/components/DailyWeather.js
@@ -19,16 +19,17 @@ function DailyWeather() {
       
       {weather.daily.map((day, index)=>{
        
-          return   <Link style={{textDecoration: "none"}} key={index} to={`./routes/Day${index+1}` } > 
+          return   <Link style={{textDecoration: "none"}} key={index} to={`./routes/Day${index}` } > 
                 <div  className='card-container'>              
-                <div  className='day-day'>{moment().add(`${index+1}`, 'days').format('dddd') }</div>
+                <div  className='day-day'>{moment().add(`${index}`, 'days').format('dddd') }</div>
                 <div className='day-degree'> {Math.round(day.temp.max)}°C
                 / {Math.round(day.temp.min)}°C</div> 
-                <div className='day-icon'><img className='daily-icons'  src={`http://openweathermap.org/img/w/${weather.daily[index].weather[0].icon}.png`} alt="" />
-                </div>
+                <div className='day-icon'><img className='daily-icons'  src={`http://openweathermap.org/img/w/${weather.daily[index].weather[0].icon}.png`} alt="" /></div>
+                <div className='description'>{weather.daily[index].weather[0].description}</div>
                 </div>
                     </Link>
       })}
+        {/* {weather.daily[0].dt} */}
         
       </div>
       <Outlet/>
