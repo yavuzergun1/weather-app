d305b03a63aa1d0d08bc9e7fee153d859cc80f15
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-05-31 22:03:38 +0300

    commited

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import React from 'react';
 import Container from './components/Container';
 import './App.css'
 import { CityDataProvider } from './context/CityDataContext';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 import Day from './routes/Day';
 import DailyWeather from './components/weeklyWeather';
 function App() {
   return (
     <CityDataProvider> 
-    <BrowserRouter> 
+    <HashRouter> 
     <Routes> 
       <Route path="/weather-app" element={<Container/>} />
       <Route path="/Days" element={<DailyWeather/>} />
       <Route path=":id" element={<Day/>} />
     
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
     </CityDataProvider>
   )
 }
